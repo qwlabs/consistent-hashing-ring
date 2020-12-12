2f152a7a8c555f30ad3d6543f68fb93063935b56
pipinet <pipinet@vip.qq.com>
2020-12-12 19:06:09 +0800

    fix code

--- core/src/test/java/com/qwlabs/ring/ConsistentHashingRingTest.java
+++ core/src/test/java/com/qwlabs/ring/ConsistentHashingRingTest.java
@@ -2,6 +2,7 @@ package com.qwlabs.ring;
 
 
 import net.openhft.hashing.LongHashFunction;
+import org.checkerframework.org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,25 +19,33 @@ public class ConsistentHashingRingTest {
 
     @Test
     void test_remove() {
-        for (int index = 0; index <= 100; index++) {
-            ring.add("node" + index);
+        for (int index = 0; index <= 10; index++) {
+            ring.add(createNode());
         }
-        Set<String> nodes = ring.replicateNodes("1", 3);
+        Set<String> nodes = ring.replicateNodes("1", 5);
         System.out.println(nodes);
-        ring.remove(nodes.iterator().next());
-        System.out.println(ring.replicateNodes("1", 3));
+        String removeNode = nodes.iterator().next();
+        System.out.println("remove node:" + removeNode);
+        ring.remove(removeNode);
+        System.out.println(ring.replicateNodes("1", 5));
         System.out.println(ring);
     }
 
     @Test
     void test_add() {
-        for (int index = 0; index <= 50; index++) {
-            ring.add("node" + index);
+        for (int index = 0; index <= 10; index++) {
+            ring.add(createNode());
         }
-        Set<String> nodes = ring.replicateNodes("1", 5);
+        Set<String> nodes = ring.replicateNodes("1", 3);
         System.out.println(nodes);
-        ring.add("node101");
-        System.out.println(ring.replicateNodes("1", 5));
+        String createNode = createNode();
+        System.out.println("add node:" + createNode);
+        ring.add(createNode);
+        System.out.println(ring.replicateNodes("1", 3));
         System.out.println(ring);
+    }
+
+    private String createNode(){
+        return RandomStringUtils.randomAlphanumeric(5);
     }
 }
